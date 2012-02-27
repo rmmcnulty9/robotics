1e40b540763912823db69ddb06a2392dceba6bdb
Greg Seaman <gregory.seaman@gmail.com>
2012-02-27 12:28:52 -0500

    added maze_nav as main program for assig 2

---
+++ maze_navigator.cpp
@@ -0,0 +1,32 @@
+//The start of a program that uses 
+
+#include <stdio.h>
+#include <robot_if++.h>
+#include <iostream>
+#include <string>
+#include "shared_constants.h"
+#include "RobotPose.cpp"
+#include "PIDController.h"
+extern "C" {
+#include "Kalman/kalmanFilterDef.h"
+}
+
+int main(int argv, char **argc)
+{
+	// Make sure we have a valid command line argument
+	if(argv <= 1) {
+		std::cout << "Usage: robot_test <address of robot> " << std::endl;
+		exit(-1);
+	}
+    
+	RobotInterface *robot = new RobotInterface(argc[1],0);
+	
+	RobotPose robotPose(robot);
+	
+	
+	delete(robot);
+	return 0;
+
+
+}
+
