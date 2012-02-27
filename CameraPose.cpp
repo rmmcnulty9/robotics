77adae097ca762100f7f76a96c03a79ba6752a5c
Greg Seaman <gregory.seaman@gmail.com>
2012-02-27 14:26:08 -0500

    changing square stuff

--- CameraPose.cpp
+++ CameraPose.cpp
@@ -12,16 +12,16 @@
 
 CameraPose::CameraPose(RobotInterface *r){
 	robot = r;
-	image = cvCreateImage(cvSize(320, 240), IPL_DEPTH_8U, 3);
-	hsv = cvCreateImage(cvSize(320, 240), IPL_DEPTH_8U, 3);
-	filtered = cvCreateImage(cvSize(320, 240), IPL_DEPTH_8U, 1);
+	image = cvCreateImage(cvSize(640, 480), IPL_DEPTH_8U, 3);
+	hsv = cvCreateImage(cvSize(640, 480), IPL_DEPTH_8U, 3);
+	filtered = cvCreateImage(cvSize(640, 480), IPL_DEPTH_8U, 1);
 
 	cvNamedWindow("Unfiltered", CV_WINDOW_AUTOSIZE);
 	cvNamedWindow("HSV", CV_WINDOW_AUTOSIZE);
 	cvNamedWindow("Filtered", CV_WINDOW_AUTOSIZE);
 
 	// Setup the camera
-        if(robot->CameraCfg(RI_CAMERA_DEFAULT_BRIGHTNESS, RI_CAMERA_DEFAULT_CONTRAST, 5, RI_CAMERA_RES_320, RI_CAMERA_QUALITY_HIGH)) {
+        if(robot->CameraCfg(RI_CAMERA_DEFAULT_BRIGHTNESS, RI_CAMERA_DEFAULT_CONTRAST, 5, RI_CAMERA_RES_640, RI_CAMERA_QUALITY_HIGH)) {
                 std::cout << "Failed to configure the camera!" << std::endl;
                 exit(-1);
         }
@@ -35,25 +35,26 @@ void CameraPose::updateCamera(){
 	robot->getImage(image);
 	// Convert to hsv
 	cvCvtColor(image, hsv, CV_BGR2HSV);
-	cvInRangeS(hsv, RC_PINK_LOW, RC_PINK_HIGH, filtered);
-	findSquares(filtered, CV_RGB(255,0,0));	
+
 	cvInRangeS(hsv, RC_YELLOW_LOW, RC_YELLOW_HIGH, filtered);
 	findSquares(filtered, CV_RGB(0,255,0));
+	
+	cvInRangeS(hsv, RC_PINK_LOW, RC_PINK_HIGH, filtered);
+	findSquares(filtered, CV_RGB(255,0,0));	
 	
 	displayImage();
 }
 
 void CameraPose::displayImage(){
 	cvShowImage("Unfiltered", image);
-	cvShowImage("HSV", hsv);
 	cvShowImage("Filtered", filtered);
-	cvWaitKey(25);
+	cvWaitKey(1000);
 }
 
 void CameraPose::findSquares(IplImage *f, CvScalar color){
 	squares_t *squares;
 	CvPoint pt1, pt2;
-	squares = robot->findSquares(filtered, 10);
+	squares = robot->findSquares(filtered, 250);
 	while(squares != NULL) {
         
 			int sq_amt = (int) (sqrt(squares->area) / 2);
